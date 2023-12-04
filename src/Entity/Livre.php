14dfb5d7a9173e640c7842fdade738cc69854987
Clara <clara.delhoute@uphf.fr>
2023-12-04 16:18:20 +0100

    Ajout classe Livre

---
+++ src/Entity/Livre.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Acgroup\MyBooks\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\LivreRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Date;
+
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+#[ORM\Table(name: 'livres')]
+class Livre
+{
+   #[ORM\Id]
+   #[ORM\Column(type:'integer')]
+   #[ORM\GeneratedValue]
+    private int|null $id = null;
+   #[ORM\Column(type : 'string')]
+   #[Assert\NotBlank]
+    private String $titre;
+    private String $auteur;
+    private Date $anneePublication;
+    private String $editeur;
+    private String $ISBN;
+
+    public function __construct(int $id, String $titre, String $auteur, Date $anneePublication, String $editeur, String $ISBN ){
+        $this->id = $id;
+        $this->titre = $titre;
+        $this->auteur = $auteur;
+        $this->anneePublication = $anneePublication;
+        $this->editeur = $editeur;
+        $this->ISBN = $ISBN;
+    }
+
+ /*
+ * @return int|null
+ */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTItre(): ?string{
+        return $this->titre;
+    }
+
+    public function setTitre(String $titre) : void{
+        $this->titre = $titre;
+    }
+
+    public function getAuteur(): ?string{
+        return $this->auteur;
+    }
+
+    public function setAuteur(String $auteur) : void{
+        $this->auteur = $auteur;
+    }
+
+    public function getAnneePublication(): ?Date{
+        return $this->anneePublication;
+    }
+
+    public function setAnneePublication(Date $anneePublication) : void{
+        $this->anneePublication = $anneePublication;
+    }
+
+    public function getEditeur(): ?string{
+        return $this->editeur;
+    }
+
+    public function setEditeur(String $editeur):void{
+        $this->editeur = $editeur;
+    }
+
+    public function getISBN():?string{
+        return $this->ISBN;
+    }
+
+    public function setISBN(string $isbn):void{
+        $this->ISBN = $isbn;
+    }
+}
